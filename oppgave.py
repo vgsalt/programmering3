365d6b4c133935b883bed2d30a238760f75700b0
Alexander <144659394+vgsalt@users.noreply.github.com>
2023-12-13 13:45:33 +0100

    Jo

--- oppgave.py
+++ oppgave.py
@@ -1,5 +1,5 @@
 import click
-from PIL import Image
+from PIL import Image, ImageOps, ImageDraw, ImageFont
 import pilgram
 import pilgram.css
 
@@ -41,9 +41,76 @@ def resize(image, width, height):
     with Image.open(image) as im:
         im.resize((int(width), int(height))).show()
 
+
+@click.command()
+@click.option('--image', help="Bildet som skal brukes", prompt=True)
+@click.option('--type', help="Hva bildet skal se ut som. Valg: square, landscape, portrait, story", prompt=True)
+def cropforinsta(image, type):
+    """Beskjær bildet etter instagram sine krav"""
+    with Image.open(image) as im:
+        if type == "square":
+            im.resize((1080, 1080)).show()
+        elif type == "landscape":
+            im.resize((1080, 608)).show()
+        elif type == "portrait":
+            im.resize((1080, 1350)).show()
+        elif type == "story":
+            # Dette skal beholde bildeformatet
+            # https://stackoverflow.com/a/4271003
+            im.thumbnail((1080, 1920), Image.LANCZOS)
+            im.show()
+        else:
+            print("Ukjent type.")
+
+@click.command()
+@click.option('--image', help="Bildet som skal brukes", prompt=True)
+def vintageify(image):
+    """Få bildet til å se litt gammelt ut 😎🧓🏻"""
+    with Image.open(image) as im:
+        # Willow ser litt vintage ut
+        im = pilgram.willow(im)
+        im.thumbnail((500, 500), Image.LANCZOS)
+        # topp, høyre, bunn og venstre
+        border = (20, 20, 20, 120)
+        # Farge rundt bildet
+        farge = "#EDD9E8"
+        nytt_bilde = ImageOps.expand(im, border=border, fill=farge)
+
+        nytt_bilde.show()
+
+@click.command()
+@click.option('--image', help="Bildet som skal brukes", prompt=True)
+@click.option('--text', help="Tekst som skal på bildet", prompt=True)
+def tekst(image, text):
+    """Putt på litt tekst på bildet"""
+    with Image.open(image) as im:
+        # Tegn den greia vi gjorde i vintageify
+        im.thumbnail((500, 500), Image.LANCZOS)
+        border = (20, 20, 20, 120)
+        farge = "#EDD9E8"
+        nytt_bilde = ImageOps.expand(im, border=border, fill=farge)
+
+        # Tegn tekst
+        font = ImageFont.truetype("Afacad-VariableFont_wght.ttf", 30)
+        ImageDraw.Draw(
+            nytt_bilde
+        ).text(
+            (20, 560),
+            text,
+            (0, 0, 0),
+            font=font,
+        )
+
+        nytt_bilde.show()
+
+# Legg til alle kommandoene
+commands.add_command(tekst)
+commands.add_command(vintageify)
 commands.add_command(rotate)
 commands.add_command(applyfilter)
 commands.add_command(resize)
+commands.add_command(cropforinsta)
 
+# Når du kjører oppgave.py så kjører den commands()
 if __name__ == '__main__':
     commands()
